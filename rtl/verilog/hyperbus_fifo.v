ffe088cc0868a1cc71a44f0c032f2b31a1441f12
fuzz <mthompson@hexwave.com>
2020-12-17 13:45:18 -0800

    test

--- rtl/verilog/hyperbus_fifo.v
+++ rtl/verilog/hyperbus_fifo.v
@@ -191,7 +191,8 @@ always @(posedge hbus_clk or posedge hbus_rst) begin
                     rx_winc <= 1'b1;
                     state <= STATE_IDLE;
                 end else begin
-                    if(hbus_valid && rx_rempty) begin
+                    //if(hbus_valid && rx_rempty) begin
+                    if(hbus_valid) begin
                         if(count == 1) begin
                             hbus_rrq <= 1'b0;
                         end
